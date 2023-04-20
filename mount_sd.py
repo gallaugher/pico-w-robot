c9b66be3ef51c602464b38dec454c9b6474aa05c
Gallaugher <john.gallaugher@bc.edu>
2023-04-20 17:11:42 -0400

    Create mount_sd.py

---
+++ mount_sd.py
@@ -0,0 +1,13 @@
+# mount_sd.py will mount an SD card volume
+# assumes wiring as shown below.
+# works with card: https://www.adafruit.com/product/4682
+import board, busio, sdcardio, storage
+# setup pins for SPI
+sck = board.GP10 # yellow
+si = board.GP11 # blue
+so = board.GP12 # green
+cs = board.GP13 # yellow
+spi = busio.SPI(sck, si, so)
+sdcard = sdcardio.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
